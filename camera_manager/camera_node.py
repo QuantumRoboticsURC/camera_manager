2481c983ce5c6bc6e54bd8027dd7bc5b6a39e251
Teze14 <jesus2004cel@gmail.com>
2024-02-01 18:27:45 -0600

    update

--- camera_manager/camera_node.py
+++ camera_manager/camera_node.py
@@ -8,17 +8,16 @@ from cv_bridge import CvBridge
 import numpy as np
 import time
 import cv2
-import pyzed.sl as sl
 import math
 
 class Camera(Node):
 	
 	def __init__(self):
-		super().__init__("Camera Node")
+		super().__init__("Camera_Node")
 		self.arm_cam_publisher = self.create_publisher(Image,"/arm_cam",10)
 		self.ant_cam_publisher = self.create_publisher(Image,"/ant_cam",10)
-		self.arm_camera = cv2.VideoCapture("/dev/video0")
-		self.antenna_camera = cv2.VideoCapture("/dev/video1")
+		self.arm_camera = cv2.VideoCapture(0)
+		self.antenna_camera = cv2.VideoCapture(3)
 		self.quality = 18
 		self.timer = self.create_timer(0.0001,self.cameras)
 		self.bridge = CvBridge()
@@ -37,9 +36,10 @@ class Camera(Node):
 		return msg
 	
 	def cameras(self):
+		print('tttttttt')
 		ret_arm,frame_arm = self.arm_camera.read()
 		ret_ant,frame_ant = self.antenna_camera.read()
-		
+		print(ret_arm,ret_ant)
 		if ret_arm:
 			self.arm_cam_publisher.publish(self.cv2_to_imgmsg_resized(frame_arm,self.quality))
    
