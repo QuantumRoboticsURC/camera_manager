1eccf98167407c1cde0a06b2c613a4cd982591da
A01798380 <ulises.jp@outlook.com>
2024-05-28 11:08:50 -0600

    Add camera selection via topic

--- camera_manager/camera_node.py
+++ camera_manager/camera_node.py
@@ -1,73 +1,77 @@
 import rclpy
+from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from std_msgs.msg import Int8
-import time
-import math
 import cv2
 
 class Camera(Node):
-	def __init__(self):
-		super().__init__("Camera_Node")
-		self.arm_cam_publisher = self.create_publisher(Image, "/arm_cam", 10)
-		self.ant_cam_publisher = self.create_publisher(Image, "/ant_cam", 10)
-		self.create_subscription(Int8, "image_quality", self.quality_callback, 1)
-		self.quality = 18
-		self.bridge = CvBridge()
-		self.timer = self.create_timer(0.0001, self.cameras)
+    def __init__(self):
+        super().__init__("Camera_Node")
+        listener_group = ReentrantCallbackGroup()
+        self.arm_cam_publisher = self.create_publisher(Image, "/arm_cam", 10)
+        self.ant_cam_publisher = self.create_publisher(Image, "/ant_cam", 10)
+        self.create_subscription(Int8, "image_quality", self.quality_callback, 1)
+        self.create_subscription(Int8, "/selected_camera", self.update_camera, 1, callback_group=listener_group)
+        self.quality = 18
+        self.camera = -1  
+        self.bridge = CvBridge()
+        self.timer = self.create_timer(0.0001, self.cameras)
 
-		# Inicializar cámaras
-		self.arm_camera = self.initialize_camera()
-		self.antenna_camera = self.initialize_camera()
+        # Inicializar cámaras
+        self.arm_camera = self.initialize_camera()
+        self.antenna_camera = self.initialize_camera()
   
-	def quality_callback(self, msg):
-		self.quality = msg.data
+    def quality_callback(self, msg):
+        self.quality = msg.data
   
-	def initialize_camera(self):
-		num_cameras = 5  # Rango de posibles indices generados
-		for index in range(num_cameras):
-			camera = cv2.VideoCapture(index)
-			if camera.isOpened():
-				self.get_logger().info(f"Cámara encontrada en el índice {index}")
-				return camera
-			camera.release()
+    def initialize_camera(self):
+        num_cameras = 5  # Rango de posibles índices generados
+        for index in range(num_cameras):
+            camera = cv2.VideoCapture(index)
+            if camera.isOpened():
+                self.get_logger().info(f"Cámara encontrada en el índice {index}")
+                return camera
+            camera.release()
 
-		self.get_logger().error("No se encontraron cámaras disponibles.")
-		return None
+        self.get_logger().error("No se encontraron cámaras disponibles.")
+        return None
 
-	def cv2_to_imgmsg(self, image):
-		msg = self.bridge.cv2_to_imgmsg(image, encoding = "bgr8")
-		return msg
+    def cv2_to_imgmsg(self, image):
+        return self.bridge.cv2_to_imgmsg(image, encoding="bgr8")
 
-	def cv2_to_imgmsg_resized(self, image, scale_percent):
-		width = int(image.shape[1] * scale_percent / 100)
-		height = int(image.shape[0] * scale_percent / 100)
-		dim = (width, height)
-		resized_image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-		msg = self.bridge.cv2_to_imgmsg(resized_image, encoding = "bgr8")
-		return msg
+    def cv2_to_imgmsg_resized(self, image, scale_percent):
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+        return self.bridge.cv2_to_imgmsg(resized_image, encoding="bgr8")
 
-	def cameras(self):
-		if self.arm_camera:
-			ret_arm, frame_arm = self.arm_camera.read()
-			if ret_arm:
-				self.arm_cam_publisher.publish(self.cv2_to_imgmsg_resized(frame_arm, self.quality))
+    def update_camera(self, msg):
+        self.camera = msg.data
 
-		if self.antenna_camera:
-			ret_ant, frame_ant = self.antenna_camera.read()
-			if ret_ant:
-				self.ant_cam_publisher.publish(self.cv2_to_imgmsg_resized(frame_ant, self.quality))
+    def cameras(self):
+        if self.arm_camera and self.camera == 0:
+            ret_arm, frame_arm = self.arm_camera.read()
+            if ret_arm:
+                self.arm_cam_publisher.publish(self.cv2_to_imgmsg_resized(frame_arm, self.quality))
+
+        elif self.antenna_camera and self.camera == 1:
+            ret_ant, frame_ant = self.antenna_camera.read()
+            if ret_ant:
+                self.ant_cam_publisher.publish(self.cv2_to_imgmsg_resized(frame_ant, self.quality))
 
 def main(args=None):
-	rclpy.init(args=args)
-	cameras = Camera()
-	rclpy.spin(cameras)
-	cameras.zed.close()
-	cameras.destroy_node()
-	rclpy.shutdown()
+    rclpy.init(args=args)
+    cameras = Camera()
+    rclpy.spin(cameras)
+    if cameras.arm_camera:
+        cameras.arm_camera.release()
+    if cameras.antenna_camera:
+        cameras.antenna_camera.release()
+    cameras.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == "__main__":
-	main()
-			
-			
+    main()
--- camera_manager/science_cameras.py
+++ camera_manager/science_cameras.py
@@ -1,71 +1,77 @@
 import rclpy
+from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from std_msgs.msg import Int8
-import time
-import math
 import cv2
 
 class Camera(Node):
-	def __init__(self):
-		super().__init__("Camera_Node")
-		self.sci_cam1_publisher = self.create_publisher(Image, "/sci_cam1", 10)
-		self.sci_cam2_publisher = self.create_publisher(Image, "/sci_cam2", 10)
-		self.create_subscription(Int8, "image_quality", self.quality_callback, 1)
-		self.quality = 18
-		self.bridge = CvBridge()
-		self.timer = self.create_timer(0.0001, self.cameras)
+    def __init__(self):
+        super().__init__("Camera_Node")
+        listener_group = ReentrantCallbackGroup()
+        self.sci_cam1_publisher = self.create_publisher(Image, "/sci_cam1", 10)
+        self.sci_cam2_publisher = self.create_publisher(Image, "/sci_cam2", 10)
+        self.create_subscription(Int8, "image_quality", self.quality_callback, 1)
+        self.create_subscription(Int8, "/selected_camera", self.update_camera, 1, callback_group=listener_group)
+        self.quality = 18
+        self.camera = -1 
+        self.bridge = CvBridge()
+        self.timer = self.create_timer(0.0001, self.cameras)
 
-		# Inicializar cámaras
-		self.sci_camera1 = self.initialize_camera()
-		self.sci_camera2 = self.initialize_camera()
+        # Inicializar cámaras
+        self.sci_camera1 = self.initialize_camera()
+        self.sci_camera2 = self.initialize_camera()
+
+    def quality_callback(self, msg):
+        self.quality = msg.data
   
-	def quality_callback(self, msg):
-		self.quality = msg.data
-  
-	def initialize_camera(self):
-		num_cameras = 5  # Rango de posibles indices generados
-		for index in range(num_cameras):
-			camera = cv2.VideoCapture(index)
-			if camera.isOpened():
-				self.get_logger().info(f"Cámara encontrada en el índice {index}")
-				return camera
-			camera.release()
+    def initialize_camera(self):
+        num_cameras = 5  # Rango de posibles índices generados
+        for index in range(num_cameras):
+            camera = cv2.VideoCapture(index)
+            if camera.isOpened():
+                self.get_logger().info(f"Cámara encontrada en el índice {index}")
+                return camera
+            camera.release()
 
-		self.get_logger().error("No se encontraron cámaras disponibles.")
-		return None
+        self.get_logger().error("No se encontraron cámaras disponibles.")
+        return None
 
-	def cv2_to_imgmsg(self, image):
-		msg = self.bridge.cv2_to_imgmsg(image, encoding = "bgr8")
-		return msg
+    def cv2_to_imgmsg(self, image):
+        return self.bridge.cv2_to_imgmsg(image, encoding="bgr8")
 
-	def cv2_to_imgmsg_resized(self, image, scale_percent):
-		width = int(image.shape[1] * scale_percent / 100)
-		height = int(image.shape[0] * scale_percent / 100)
-		dim = (width, height)
-		resized_image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-		msg = self.bridge.cv2_to_imgmsg(resized_image, encoding = "bgr8")
-		return msg
+    def cv2_to_imgmsg_resized(self, image, scale_percent):
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+        return self.bridge.cv2_to_imgmsg(resized_image, encoding="bgr8")
 
-	def cameras(self):
-		if self.sci_camera1:
-			ret_sci1, frame_sci1 = self.sci_camera1.read()
-			if ret_sci1:
-				self.sci_cam1_publisher.publish(self.cv2_to_imgmsg_resized(frame_sci1, self.quality))
+    def update_camera(self, msg):
+        self.camera = msg.data
 
-		if self.sci_camera2:
-			ret_sci2, frame_sci2 = self.sci_camera2.read()
-			if ret_sci2:
-				self.sci_cam2_publisher.publish(self.cv2_to_imgmsg_resized(frame_sci2, self.quality))
+    def cameras(self):
+        if self.sci_camera1 and self.camera == 0:
+            ret_sci1, frame_sci1 = self.sci_camera1.read()
+            if ret_sci1:
+                self.sci_cam1_publisher.publish(self.cv2_to_imgmsg_resized(frame_sci1, self.quality))
+
+        elif self.sci_camera2 and self.camera == 1:
+            ret_sci2, frame_sci2 = self.sci_camera2.read()
+            if ret_sci2:
+                self.sci_cam2_publisher.publish(self.cv2_to_imgmsg_resized(frame_sci2, self.quality))
 
 def main(args=None):
-	rclpy.init(args=args)
-	cameras = Camera()
-	rclpy.spin(cameras)
-	cameras.zed.close()
-	cameras.destroy_node()
-	rclpy.shutdown()
+    rclpy.init(args=args)
+    cameras = Camera()
+    rclpy.spin(cameras)
+    if cameras.sci_camera1:
+        cameras.sci_camera1.release()
+    if cameras.sci_camera2:
+        cameras.sci_camera2.release()
+    cameras.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == "__main__":
-	main()
+    main()
